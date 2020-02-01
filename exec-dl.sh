8a5ec551fc4723293bf9c9842e48442cc8b8a26d
Irvin Capagcuan <irvin@capagcuan.org>
2020-02-01 10:05:00 +0800

    update

--- exec-dl.sh
+++ exec-dl.sh
@@ -2,6 +2,6 @@
 COURSE=$1
 
 #pip install -r requirements.txt
-python udemy-dl.py --skip-sub -o ./boon-tuts-vids --cookies cookie.txt https://lamudigroup.udemy.com/${COURSE}
+python udemy-dl.py --skip-sub -o ./boon-tut-vids --cookies cookie.txt https://lamudigroup.udemy.com/${COURSE}
 
 aws s3 cp --recursive ./boon-tut-vids/${COURSE} s3://boon-tut-vids/${COURSE}
