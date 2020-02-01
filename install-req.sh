05cefa1091cc31c5059a594facce0ce88c864091
Irvin Capagcuan <irvin@capagcuan.org>
2020-02-01 09:49:54 +0800

    update

--- install-req.sh
+++ install-req.sh
@@ -2,3 +2,5 @@
 
 sudo apt update; sudo apt install python-pip git -y; pip3 install awscli --upgrade --user; git clone https://github.com/travis-south/udemy-dl.git; cd udemy-dl;
 # Run export for aws keys
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
