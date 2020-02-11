4352dfbb26aeb72ce1ce50626447c23d32292613
Irvin Capagcuan <irvin@capagcuan.org>
2020-02-11 15:58:12 +0800

    updates

--- install-req.sh
+++ install-req.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt update; sudo apt install python-pip git -y; pip install awscli --upgrade --user; git clone https://github.com/travis-south/udemy-dl.git; cd udemy-dl;
+sudo apt update; sudo apt install python-pip git -y; pip install awscli --upgrade --user; git clone https://github.com/travis-south/udemy-dl.git; cd udemy-dl; export PATH=$PATH:/home/ubuntu/.local/bin;
 
 AWS_ACCESS_KEY_ID= AWS_SECRET_ACCESS_KEY= ./dl-all.sh > dl.log 2>&1 &
